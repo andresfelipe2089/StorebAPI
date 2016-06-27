0336a7ec9c59714c774fabbaf953459352fd47e8
andres <andresfe2089@hotmail.com>
2016-06-27 13:06:02 -0500

    added a specfor the organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+   it "orders by title" do
+    company = Organization.create!(title: "company", description: "ninguna", type: "basuras")
+    empresa = Organization.create!(title: "empresa", description: "ninguna", type: "fiestas")
+
+    expect(Organization.ordered_by_title).to eq([company, empresa])
+  end
